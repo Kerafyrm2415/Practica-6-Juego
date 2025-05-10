9c0d02a9303a9c42252c915e88a431e173cd419b
derek <112148047+Kerafyrm2415@users.noreply.github.com>
2025-05-10 10:12:46 -0700

    Add files via upload

---
+++ Enemigo.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public abstract class Enemigo extends Entidad {
+    public Enemigo(int x, int y, int ancho, int alto) {
+        super(x, y, ancho, alto);
+    }
+}
---
+++ EnemigoVolador.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class EnemigoVolador extends Enemigo {
+    public EnemigoVolador(int x, int y, int ancho, int alto) {
+        super(x, y, ancho, alto);
+    }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.MAGENTA);
+        g.fillOval(x, y, ancho, alto);
+    }
+}
